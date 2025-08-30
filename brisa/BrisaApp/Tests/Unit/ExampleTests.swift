4926cd6ceae94835f1effc063ce949cf409164af
Alexandr Krikun <44290517+ksanyok@users.noreply.github.com>
2025-08-31 02:09:29 +0300

    update

--- brisa/BrisaApp/Tests/Unit/ExampleTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-
-/// Пример юнит‑теста для Brisa. В будущем сюда будут добавляться настоящие тесты модулей.
-final class ExampleTests: XCTestCase {
-    func testAlwaysTrue() {
-        XCTAssertTrue(true)
-    }
-}
