9219aee6a1690a46c7369cf9dab0181fc16cf732
Alexandr Krikun <44290517+ksanyok@users.noreply.github.com>
2025-09-06 17:20:58 +0300

    clear

--- BrisaApp/Sources/Update/Updater.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Менеджер обновлений приложения. В production версии интегрируется с Sparkle.
-final class Updater {
-    func checkForUpdates() {
-        // TODO: инициализировать Sparkle и проверить наличие обновлений
-    }
-}
--- BrisaApp/Tests/Integration/ExampleIntegrationTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-
-/// Пример интеграционного теста. В рамках MVP тесты могут быть пустыми.
-final class ExampleIntegrationTests: XCTestCase {
-    func testIntegrationPlaceholder() {
-        XCTAssert(true, "Интеграционный тест пока не реализован")
-    }
-}
